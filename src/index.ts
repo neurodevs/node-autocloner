e3b22ff1fb51f19e6d3d3ff9c5a528ceba7ed76f
ericthecurious <hello@ericthecurious.com>
2024-12-01 23:56:20 -0600

    major: update exports and release v1.0.0

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-//exports go here
+export { default as GitAutocloner } from './components/GitAutocloner'
+export * from './components/GitAutocloner'
+
+export { default as NeurodevsAutocloner } from './components/NeurodevsAutocloner'
+export * from './components/NeurodevsAutocloner'
+
+export { default as FakeGitAutocloner } from './testDoubles/FakeGitAutocloner'
+export * from './testDoubles/FakeGitAutocloner'
+
+export * from './abstract.types'
